4591567c141df162da6c106fdadc5ace34fb8e4b
CHOPSTICKLOL <siddiquesameer007@gmail.com>
2025-11-09 15:48:34 +0000

    v

--- week 9/ex1.py
+++ week 9/ex1.py
@@ -1,5 +1,5 @@
-l1=[1,2,3,4,5,7]
-l2=[1,2,3,4,5,8]
-for number in l1:
-    if number in l2:
-      print(number)
+lst1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+lst2=[1,2,3,4,5,10,30]
+for number in lst1:
+    if number in lst2:
+        print(number)
